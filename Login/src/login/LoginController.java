48d20fbefe3a44b6c1f26da1f70f291525e98810
devcation <devcation@gmail.com>
2022-02-04 21:35:42 +0900

    start

---
+++ Login/src/login/LoginController.java
@@ -0,0 +1,97 @@
+package login;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private UserDAO dao;
+	private ServletContext ctx;
+	
+	private final String START_PAGE = "login.jsp";
+      
+	
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		dao = new UserDAO();
+		ctx = getServletContext();		
+	}
+	
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("action");
+		
+		dao = new UserDAO();
+		
+		Method m;
+		String view = null;
+		
+		if (action == null) {
+			action = "signIn";
+		}
+		
+		try {
+			
+			m = this.getClass().getMethod(action, HttpServletRequest.class);
+			view = (String)m.invoke(this, request);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+	
+			ctx.log("요청 action 없음!!");
+			request.setAttribute("error", "action 파라미터가 잘못 되었습니다!!");
+			view = START_PAGE;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	
+		if(view.startsWith("redirect:/")) {
+
+			String rview = view.substring("redirect:/".length()); 
+			response.sendRedirect(rview);
+		} else {
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+			dispatcher.forward(request, response);	
+		}
+	}
+
+	
+	public String signIn(HttpServletRequest request) {
+		return START_PAGE;
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
